0558b8b268dfc4c95dc34f81a69056ab3a70855d
UHquokka <bananamayong@gmail.com>
2023-04-07 19:14:35 +0900

    audio - play pause 버튼 만들기

--- src/client/js/musicPlayer.js
+++ src/client/js/musicPlayer.js
@@ -1,9 +1,19 @@
 const audio = document.querySelector("audio");
 const musicPlayer = document.getElementById("musicPlayer");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
 
-const handlePlay = async () => {
+const handleViews = async () => {
   const { id } = musicPlayer.dataset;
   await fetch(`/api/music/${id}/view`, { method: "POST" });
 };
 
-audio.addEventListener("play", handlePlay);
+const handlePlayClick = (e) => {
+  audio.paused ? audio.play() : audio.pause();
+  playBtn.innerText = audio.paused ? "Play" : "Pause";
+};
+
+audio.addEventListener("play", handleViews);
+playBtn.addEventListener("click", handlePlayClick);
