325b666da95fc2aeb0823280783dc109595d2809
UHquokka <bananamayong@gmail.com>
2023-04-09 21:24:54 +0900

    play 화면 icon 변경, volume bar 아이콘 hover 시 보임

--- src/client/js/musicPlayer.js
+++ src/client/js/musicPlayer.js
@@ -1,10 +1,13 @@
 const audio = document.querySelector("audio");
 const musicPlayer = document.getElementById("musicPlayer");
 const playBtn = document.getElementById("play");
-const muteBtn = document.getElementById("mute");
+const playBtnIcon = document.getElementById("playBtnIcon");
+const muteBtn = document.getElementById("muteBtn");
+const muteBtnIcon = document.getElementById("muteBtnIcon");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 audio.volume = volumeValue;
@@ -16,16 +19,26 @@ const handleViews = async () => {
 
 const handlePlayClick = (e) => {
   audio.paused ? audio.play() : audio.pause();
-  playBtn.innerText = audio.paused ? "Play" : "Pause";
+  if (audio.paused) {
+    playBtn;
+    playBtnIcon.classList.remove("fa-pause-circle");
+    playBtnIcon.classList.add("fa-play-circle");
+  } else {
+    playBtnIcon.classList.remove("fa-play-circle");
+    playBtnIcon.classList.add("fa-pause-circle");
+  }
 };
 
 const handleMuteClick = () => {
   if (audio.muted) {
     audio.muted = false;
+    muteBtnIcon.classList.remove("fa-volume-mute");
+    muteBtnIcon.classList.add("fa-volume-up");
   } else {
     audio.muted = true;
+    muteBtnIcon.classList.remove("fa-volume-up");
+    muteBtnIcon.classList.add("fa-volume-mute");
   }
-  muteBtn.innerText = audio.muted ? "Unmuted" : "Muted";
   volumeRange.value = audio.muted ? 0 : volumeValue;
 };
 
@@ -35,7 +48,6 @@ const handleVolumeChange = (e) => {
   } = e;
   if (audio.muted) {
     audio.muted = false;
-    muteBtn.innerText = "Muted";
   }
   volumeValue = value;
   audio.volume = value;
@@ -43,6 +55,7 @@ const handleVolumeChange = (e) => {
 
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(audio.duration));
+  timeline.max = Math.floor(audio.duration);
 };
 
 const handleTimeUpdate = () => {
@@ -52,9 +65,17 @@ const handleTimeUpdate = () => {
 const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substring(14, 19);
 
+const handleTimeLine = (e) => {
+  const {
+    target: { value },
+  } = e;
+  audio.currentTime = value;
+};
+
 audio.addEventListener("play", handleViews);
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("change", handleVolumeChange);
 audio.addEventListener("loadedmetadata", handleLoadedMetadata);
 audio.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("change", handleTimeLine);
