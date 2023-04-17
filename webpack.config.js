8322564ea5d8811ca514b605e83e0b60f58c1e2d
UHquokka <bananamayong@gmail.com>
2023-04-17 20:04:38 +0900

    배경 10초마다 랜덤 로딩

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,17 @@ module.exports = {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
   },
 };
