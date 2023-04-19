175cf420bd3ad14016b10ef7dfb0c6556b61cd8d
UHquokka <bananamayong@gmail.com>
2023-04-19 22:42:49 +0900

    miniplayer에서 가장 최근에 재생한 곡 다른 page에서 재생은 성공

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   entry: {
     main: BASE_JS + "main.js",
     musicPlayer: BASE_JS + "musicPlayer.js",
+    miniplayer: BASE_JS + "miniplayer.js",
     musicHome: BASE_JS + "musicHome.js",
     community: BASE_JS + "community.js",
   },
